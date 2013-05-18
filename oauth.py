0af2d3c03befdacf8e8ef54ca9641babc30f6880
James Tomasino <james@tomasino.org>
2013-05-17 23:34:44 -0400

    trying Twython instead of oauth

--- oauth.py
+++ oauth.py
@@ -1,76 +1,44 @@
-import urlparse
-import oauth2 as oauth
+from twython import Twython
 
-request_token_url = 'https://api.twitter.com/oauth/request_token'
-access_token_url = 'https://api.twitter.com/oauth/authorize'
-authorize_url = 'https://api.twitter.com/oauth/access_token'
-consumer_key = ''
-consumer_secret = ''
-request_token = ''
+class OAuth:
 
-def init(self):
-    with open('config.txt') as config:
-    config_data = [x.strip().split(':') for x in config.readlines()]
+    request_token_url = 'https://api.twitter.com/oauth/request_token'
+    access_token_url = 'https://api.twitter.com/oauth/authorize'
+    authorize_url = 'https://api.twitter.com/oauth/access_token'
+    app_key = ''
+    app_secret = ''
+    oauth_token = ''
+    oauth_token_secret = ''
+    oauth_verifier = ''
 
-    for key,value in config_data:
-        if (key == 'consumer_key'):
-            consumer_key = value
-        elif (key == 'consumer_secret'):
-            consumer_secret = value
+    def __init__(self):
+        pass
 
-    consumer = oauth.Consumer(consumer_key, consumer_secret)
-    client = oauth.Client(consumer)
+    def get_auth(self):
+        with open('config.txt') as config:
+            config_data = [x.strip().split(':') for x in config.readlines()]
 
-# Step 1: Get a request token. This is a temporary token that is used for
-# having the user authorize an access token and to sign the request to obtain
-# said access token.
+        for key,value in config_data:
+            if (key == 'app_key'):
+                self.app_key = value
+            elif (key == 'app_secret'):
+                self.app_secret = value
 
-def getToken(self):
-    resp, content = client.request(request_token_url, "GET")
-    if resp['status'] != '200':
-        raise Exception("Invalid response %s." % resp['status'])
+    def authorize(self):
+        self.get_auth()
+        t = Twython(self.app_key, self.app_secret)
+        auth_props = t.get_authentication_tokens(
+            callback_url='http://fig.tomasino.com'
+        )
+        self.oauth_token = auth_props['oauth_token']
+        self.oauth_token_secret = auth_props['oauth_token_secret']
 
-    request_token = dict(urlparse.parse_qsl(content))
-    print "Request Token:"
-    print "    - oauth_token        = %s" % request_token['oauth_token']
-    print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
-    print
-    return request_token
+        print 'Connect to Twitter via: %s' % auth_props['auth_url']
 
-
-# Step 2: Redirect to the provider. Since this is a CLI script we do not
-# redirect. In a web application you would redirect the user to the URL
-# below.
-
-def redirect(self):
-    print "Go to the following link in your browser:"
-    print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
-    print
-
-# After the user has granted access to you, the consumer, the provider will
-# redirect you to whatever URL you have told them to redirect to. You can
-# usually define this in the oauth_callback argument as well.
-accepted = 'n'
-while accepted.lower() == 'n':
-    accepted = raw_input('Have you authorized me? (y/n) ')
-oauth_verifier = raw_input('What is the PIN? ')
-
-# Step 3: Once the consumer has redirected the user back to the oauth_callback
-# URL you can request the access token the user has approved. You use the
-# request token to sign this request. After this is done you throw away the
-# request token and use the access token returned. You should store this
-# access token somewhere safe, like a database, for future use.
-token = oauth.Token(request_token['oauth_token'],
-    request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
-client = oauth.Client(consumer, token)
-
-resp, content = client.request(access_token_url, "POST")
-access_token = dict(urlparse.parse_qsl(content))
-
-print "Access Token:"
-print "    - oauth_token        = %s" % access_token['oauth_token']
-print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
-print
-print "You may now access protected resources using the access tokens above."
-print
+    def callback(self, oauth_token, oauth_verifier):
+        self.get_auth()
+        self.oauth_token = oauth_token
+        self.oauth_verifier = oauth_verifier
+        t = Twython(self.app_key, self.app_secret,
+            self.oauth_token, self.oauth_token_secret)
+        self.auth_tokens = t.get_authorized_tokens(self.oauth_verifier)
