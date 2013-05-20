efdd1fecc11915881dd04c9467ec223380abc675
James Tomasino <james@tomasino.org>
2013-05-19 20:14:27 -0400

    fake template added for home test

--- home.py
+++ home.py
@@ -1,3 +1,27 @@
+# TODO: Replace this part with web.py template render stuff.
+PAGE_TEMPLATE = """
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+        <title>Fig - %s</title>
+        <link rel="stylesheet" href="css/main.css" type="text/css" media="screen" charset="utf-8">
+    </head>
+
+    <body>
+
+        <div class="tweets">
+        %s
+        </div>
+
+        <script type="text/javascript" src="js/main.js"></script>
+
+    </body>
+
+</html>
+"""
+
 TWEET_TEMPLATE = """
 <div class="tweet">
   <span class="twitter-user"><a href="http://twitter.com/%s">%s</a>: </span>
@@ -17,4 +41,6 @@ class Home:
         xhtml = ''
         for s in statuses:
             xhtml += TWEET_TEMPLATE % (s.user.screen_name, s.user.screen_name, s.text, s.user.screen_name, s.id, s.relative_created_at)
-        return xhtml
+
+        page = PAGE_TEMPLATE % ( 'Home', xhtml )
+        return page
